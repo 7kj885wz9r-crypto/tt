33bfe90847a4503130d68dc65c17985ac3650190
7kj885wz9r-crypto <7kj885wz9r@privaterelay.appleid.com>
2026-06-10 09:39:37 +0900

    Update tm.sh

--- tm.sh
+++ tm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo -i
-docker run -d --restart=always --name tm traffmonetizer/cli_v2 start accept --token OYdgbYFaOAMy3wdsXRj0swJiRFR6ZIbzzsGyX3cs7oM=
+wget -qO- get.docker.com | bash
+sudo bash -c 'docker run -d --restart=always --name tm traffmonetizer/cli_v2 start accept --token OYdgbYFaOAMy3wdsXRj0swJiRFR6ZIbzzsGyX3cs7oM='
